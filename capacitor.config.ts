a509790e33e9282e902475c614237fd637f33f6d
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo-2.local>
2024-06-21 11:46:06 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '01-myApp',
+  webDir: 'www'
+};
+
+export default config;
